d6d563e7c32b56c8abf8938b034eb01ff741b4de
Apeksha Sood <apeksha1396@gmail.com>
2022-01-19 23:44:38 +0530

    adding loader.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-
-
-menu.addEventListener("click", function(e) {
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-});
-
-// window.addEventListener("scroll", function(e) {
-//     menu.classList.remove('fa-times');
-//     navbar.classList.remove('active');
-// });
-
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
 }
 
-document.querySelector('#search-icons').addEventListener("click", function(e) {
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
+window.onscroll = () => {
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
+}
+
+document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
+}
 
-});
-
-
-document.querySelector('#close').addEventListener("click", function(e) {
+document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
-
-});
+}
 
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
@@ -43,3 +53,37 @@ var swiper = new Swiper(".home-slider", {
     },
     loop: true,
 });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
